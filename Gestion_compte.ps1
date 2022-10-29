24c523645f861626e655e9669970f8021c116e1b
Matthh9 <95867037+Matthh9@users.noreply.github.com>
2022-10-29 23:19:42 +0200

    Update Gestion_compte.ps1

--- Gestion_compte.ps1
+++ Gestion_compte.ps1
@@ -4,4 +4,35 @@ Date :
 
 #>
 
+#Requires -RunAsAdministrator
 
+function menu {
+    param (
+        $Texte_intruductif
+    )
+    clear
+
+    Write-Host $Texte_intruductif -ForegroundColor Green
+
+    #Write-Host ("Voulez-vous :","1 - créer un compte") -Separator "`n  " -ForegroundColor Green
+    $choix = Read-Host "Voulez-vous :`n  1 - Créer un compte`n  2 - Gestion des comptes`nChoix"
+
+    Switch ($choix){
+    1 {creation_user}
+    2 {"Cat is Mentioned"}
+    default {menu "Désolé, le choix effectué ne correspond à aucun scénario"}
+    }
+
+}
+
+
+function creation_user {
+    clear
+    Write-Host "création d'un utilisateur" -ForegroundColor Green
+}
+
+
+
+menu "Bonjour, bienvenu dans l'utilitaire de gestion des comptes locaux"
+
+#$Password = Read-Host -AsSecureString
