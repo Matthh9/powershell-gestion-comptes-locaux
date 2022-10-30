d52cc37dd14871824302b43ba9667f781033fc33
Matthh9 <95867037+Matthh9@users.noreply.github.com>
2022-10-30 16:32:31 +0100

    Update Gestion_compte.ps1

--- Gestion_compte.ps1
+++ Gestion_compte.ps1
@@ -6,29 +6,54 @@ Date :
 
 #Requires -RunAsAdministrator
 
-function menu {
+function afficher_choix {
     param (
-        $Texte_intruductif
+        $Texte_intruductif,
+        $options
     )
+    $erreur="Erreur le choix n'est pas disponible : "
+
     clear
 
     Write-Host $Texte_intruductif -ForegroundColor Green
 
-    #Write-Host ("Voulez-vous :","1 - créer un compte") -Separator "`n  " -ForegroundColor Green
-    $choix = Read-Host "Voulez-vous :`n  1 - Créer un compte`n  2 - Gestion des comptes`nChoix"
+    for($i = 0; $i -lt $options.length; $i++){ 
+        Write-Host "  "$i" - " $options[$i] 
+    }
+    $choix = Read-Host "Choix"
+    #Write-Host $choix " : " $options[$choix]
+
+    if($options -contains $options[$choix]){
+        return $choix
+    } else { afficher_choix $erreur$Texte_intruductif $options }
+
+}
+
+function menu {
+    $choix = afficher_choix "Choix d'une action" @('Créer un utilisateur', 'Modifier un utilisateur')
 
     Switch ($choix){
-    1 {creation_user}
-    2 {"Cat is Mentioned"}
-    default {menu "Désolé, le choix effectué ne correspond à aucun scénario"}
+        0 {creation_user}
+        1 {gestion_user}
     }
-
 }
 
 
 function creation_user {
     clear
     Write-Host "création d'un utilisateur" -ForegroundColor Green
+}
+
+
+function gestion_user {
+    $get_user_resultat = Get-LocalUser
+    $choix = afficher_choix "modification d'un utilisateur" $get_user_resultat
+
+    #on fait ça parce que la fonction selectionner_user retourn la valeur en double
+
+    Write-Host "user returné" $choix
+            
+
 }
 
 
