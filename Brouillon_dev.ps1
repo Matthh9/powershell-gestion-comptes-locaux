5073c0e3e33fdf215600f3220b47cb6ae719c121
Matthh9 <95867037+Matthh9@users.noreply.github.com>
2022-11-01 23:47:27 +0100

    Create Brouillon_dev.ps1

---
+++ Brouillon_dev.ps1
@@ -0,0 +1,37 @@
+﻿function test{
+    param (
+        $test2
+    )
+
+    Write-Host "Erreur le choix n'est pas disponible : "
+    $temp = (& $test2)
+    $temp
+}
+
+function Hello ($in) {
+     
+    write-Host "Hello $in"
+}
+
+function dev{
+    Write-Host "test de commande en paramètre"
+}
+
+
+function exec-script ($func, $parm, $autre) {
+       
+    #Invoke-Expression $file    
+       
+    $x = (& $func $parm)
+       
+    Write-Host $x
+
+        
+    }
+
+Write-Host "début"
+#test "Write-Host 'test de commande en paramètre'"
+Write-Host "fin"
+
+exec-script -func "Write-Host" -parm " Hello World"
+exec-script  "Get-LocalUser" "Test"
